741c69596e128651d03ef1d9e51ce1dcb6c9afba
CoreyRDean <CoreyRDean@gmail.com>
2024-10-23 16:11:26 -0500

    add plex photo cache to cleanup job

--- cleanup_old_downloads.sh
+++ cleanup_old_downloads.sh
@@ -3,6 +3,7 @@
 # Set the directory path
 MOVIE_DIR="/mnt/vault2/Downloaded/Movies"
 TV_DIR="/mnt/vault2/Downloaded/Shows"
+PHOTO_CACHE_DIR="/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Cache/PhotoTranscoder"
 
 # Set the number of days
 DAYS=3
@@ -10,7 +11,9 @@ DAYS=3
 # Find and delete files older than DAYS
 find "$MOVIE_DIR" -type f -mtime +$DAYS -delete
 find "$TV_DIR" -type f -mtime +$DAYS -delete
+find "$PHOTO_CACHE_DIR" -name "*.jpg" -type f -mtime +$DAYS -delete
 
 # Optionally, to delete empty directories as well:
 find "$MOVIE_DIR" -type d -empty -delete
 find "$TV_DIR" -type d -empty -delete
+find "$PHOTO_CACHE_DIR" -type d -empty -delete
